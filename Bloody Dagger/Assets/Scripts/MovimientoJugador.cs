408dfa66567d19aa86bf096dd96589a126f09d35
SantiagoP0815 <santiago.p.0815@gmail.com>
2024-04-20 00:44:40 -0500

    deslizar por paredes

--- Bloody Dagger/Assets/Scripts/MovimientoJugador.cs
+++ Bloody Dagger/Assets/Scripts/MovimientoJugador.cs
@@ -7,6 +7,7 @@ public class MovimientoJugador : MonoBehaviour
     private Rigidbody2D rb2D;
 
     [Header("Movimiento")]
+    private float inputX;
     private float movimientoHorizontal = 0f;
     [SerializeField] private float velocidadDeMovimiento;
     [Range(0, 0.3f)][SerializeField] private float suavizadoDeMovimiento;
@@ -17,9 +18,16 @@ public class MovimientoJugador : MonoBehaviour
     [SerializeField] private float fuerzaDeSalto;
     [SerializeField] private LayerMask queEsSuelo;
     [SerializeField] private Transform controladorSuelo;
-    [SerializeField] private Vector3 dimensionesCaja;
+    [SerializeField] private Vector3 dimensionesCajaSuelo;
     [SerializeField] private bool enSuelo;
     private bool salto = false;
+
+    [Header("DeslizarPared")]
+    [SerializeField] private Transform controladorPared;
+    [SerializeField] private Vector3 dimensionesCajaPared;
+    [SerializeField] private bool enPared;
+    [SerializeField] private bool deslizando;
+    [SerializeField] private float velocidadDeslizar;
 
     private void Start()
     {
@@ -28,42 +36,64 @@ public class MovimientoJugador : MonoBehaviour
 
     private void Update()
     {
-        movimientoHorizontal = Input.GetAxisRaw("Horizontal") * velocidadDeMovimiento;
+        inputX = Input.GetAxisRaw("Horizontal");
+        movimientoHorizontal = inputX * velocidadDeMovimiento;
 
         if (Input.GetButtonDown("Jump"))
         {
             salto = true;
         }
+
+        if(!enSuelo && enPared && inputX != 0)
+        {
+            deslizando = true;
+        }
+        else
+        {
+            deslizando= false;
+        }
     }
 
     private void FixedUpdate()
     {
-        enSuelo = Physics2D.OverlapBox(controladorSuelo.position, dimensionesCaja, 0f, queEsSuelo);
+        enSuelo = Physics2D.OverlapBox(controladorSuelo.position, dimensionesCajaSuelo, 0f, queEsSuelo);
+        enPared = Physics2D.OverlapBox(controladorPared.position, dimensionesCajaPared, 0f, queEsSuelo);
+
 
         Mover(movimientoHorizontal * Time.fixedDeltaTime, salto);
 
         salto = false;
+
+        if(deslizando) 
+        {
+            rb2D.velocity = new Vector2(rb2D.velocity.x, Mathf.Clamp(rb2D.velocity.y, -velocidadDeslizar, float.MaxValue));
+        }
     }
 
-    private void Mover(float mover, bool saltar)
+private void Mover(float mover, bool saltar)
+{
+    Vector3 velocidadObjetivo = new Vector2(mover, rb2D.velocity.y);
+    
+    // Ajustar el suavizado de movimiento dependiendo de si el jugador está deslizando o no
+    float suavizadoActual = deslizando ? 0.3f : suavizadoDeMovimiento;
+    rb2D.velocity = Vector3.SmoothDamp(rb2D.velocity, velocidadObjetivo, ref velocidad, suavizadoActual);
+
+    if ((mover > 0 && !mirandoDerecha) || (mover < 0 && mirandoDerecha))
     {
-        Vector3 velocidadObjetivo = new Vector2(mover, rb2D.velocity.y);
-        rb2D.velocity = Vector3.SmoothDamp(rb2D.velocity, velocidadObjetivo, ref velocidad, suavizadoDeMovimiento);
+        Girar();
+    }
 
-        if (mover > 0 && !mirandoDerecha)
-        {
-            Girar();
-        }
-        else if (mover < 0 && mirandoDerecha)
-        {
-            Girar();
-        }
+    if (enSuelo && saltar)
+    {
+        Salto();
+    }
+}
 
-        if (enSuelo && saltar)
-        {
-            enSuelo = false;
-            rb2D.AddForce(new Vector2(0f, fuerzaDeSalto));
-        }
+
+    private void Salto()
+    {
+        enSuelo = false;
+        rb2D.AddForce(new Vector2(0f, fuerzaDeSalto));
     }
 
     private void Girar()
@@ -77,6 +107,7 @@ public class MovimientoJugador : MonoBehaviour
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.yellow;
-        Gizmos.DrawWireCube(controladorSuelo.position, dimensionesCaja);
+        Gizmos.DrawWireCube(controladorSuelo.position, dimensionesCajaSuelo);
+        Gizmos.DrawWireCube(controladorPared.position, dimensionesCajaPared);
     }
 }
